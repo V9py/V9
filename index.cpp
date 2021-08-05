0580345dbc3015c2a832e069375eaefe0966fbe1
V9py <88491861+V9py@users.noreply.github.com>
2021-08-05 15:25:35 +0300

    Add files via upload

--- index.cpp
+++ index.cpp
@@ -8,7 +8,7 @@ int main(){
 
     V9 VM;
 
-    /* std::system("python vm_launcher.py launchcmd launchgui"); */
+    std::system("python vm_launcher.py launchcmd launchgui");
     /* VM.create_vm_instance(); */
     /* ^ is for just launching the Gui part of V9 */
 }
